1b9b410101a63a8760ac3a73933aba29c49a7fff
rishavraj <iamrishavraj1@gmail.com>
2021-08-23 22:05:34 +0530

    Final Changes Done

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-// const documentStyle = document.documentElement.style;
-// const element = document.querySelector('.awesome');
-// const dimensions = element.getBoundingClientRect();
-// const { width, height } = dimensions;
-
-// element.addEventListener('mousemove', e => {
-//   const x = e.clientX - dimensions.left;
-//   const y = e.clientY - dimensions.top;  
-//   const dx = x - width / 2;
-//   const dy = y - height / 2;
-  
-//   documentStyle.setProperty('--rx', `${dy / -1}deg`);
-//   documentStyle.setProperty('--ry', `${dx / 10}deg`);
-// });
-
-// element.addEventListener('mouseleave', e => {
-//   documentStyle.setProperty('--rx', '0');
-//   documentStyle.setProperty('--ry', '0');
-//   documentStyle.setProperty('--ty', '0');
-// });
-
-// element.addEventListener('mousedown', e => {
-//   documentStyle.setProperty('--tz', '-24px');
-// });
-
-// element.addEventListener('mouseup', e => {
-//   documentStyle.setProperty('--tz', '-12px');
-// });
-
-
 function myFunction() {
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
